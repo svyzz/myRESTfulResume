d14c1d9b8300301c13b4d5a475fe1867a7056991
svyzz <stuxnetting@gmail.com>
2015-07-24 16:10:59 +1200

    Adding all of the resource endpoints

---
+++ src/resources/contact.py
@@ -0,0 +1,1 @@
+# Ensure that this is authenticated!
---
+++ src/resources/work.py
@@ -0,0 +1,17 @@
+from flask.ext.restful import Resource
+from flask import jsonify
+
+
+class Work(Resource):
+	
+	def get(self):
+		return jsonify({'days/units' : 'm days and n units'})
+
+	def post(self, **kwargs):
+		abort(405)
+
+	def put(self, **kwargs):
+		abort(405)
+
+	def delete(self, **kwargs):
+		abort(405)
