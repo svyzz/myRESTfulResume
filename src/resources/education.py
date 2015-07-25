10f4a9653e781a88de077d250d09a6a15dbeaa3e
svyzz <stuxnetting@gmail.com>
2015-07-25 16:02:58 +1200

    Details around education added but not reviewed

--- src/resources/education.py
+++ src/resources/education.py
@@ -1,17 +1,28 @@
-from flask.ext.restful import Resource
+from flask.ext.restful import Resource, abort
 from flask import jsonify
 
 
 class Education(Resource):
 	
 	def get(self):
-		return jsonify({'days/units' : 'm days and n units'})
+		return jsonify({
+			'degree' : 'Bachelors in Engineering',
+			'major' : 'Computer Science and Engineering',
+			'university' : 'Visvesvaraya Technological University, Bangalore',
+			'year' : '2008'
+			})
+
 
 	def post(self, **kwargs):
+		"""
+    	Exit with a 405 since this method is disallowed for this resource
+		"""
 		abort(405)
+
 
 	def put(self, **kwargs):
 		abort(405)
+
 
 	def delete(self, **kwargs):
 		abort(405)
