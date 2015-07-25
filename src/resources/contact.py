280a23ca4c712bd250cdb27011813f3106cc8c08
svyzz <stuxnetting@gmail.com>
2015-07-25 16:02:34 +1200

    Contact details added but not reviewed

--- src/resources/contact.py
+++ src/resources/contact.py
@@ -1,1 +1,30 @@
-# Ensure that this is authenticated!
+from flask.ext.restful import Resource, abort
+from flask import jsonify
+
+
+class Contact(Resource):
+	
+	def get(self):
+		return jsonify({
+			'email' : '<Ask me for my resume!>',
+			'github' : 'https://github.com/svyzz *OR* /github for a smidgeon more detail!',
+			'linkedIN' : 'http://tinyurl.com/pq2lrkv',
+			'location' : 'Wellington, New Zealand',
+			'name' : 'Arun Venkatram',
+			'phone number' : '<Ask me for my resume!>'
+			})
+
+
+	def post(self, **kwargs):
+		"""
+    	Exit with a 405 since this method is disallowed for this resource
+		"""
+		abort(405)
+
+
+	def put(self, **kwargs):
+		abort(405)
+
+
+	def delete(self, **kwargs):
+		abort(405)
