6490057603c3988f2f7ac9d1308e3b42928034ad
svyzz <stuxnetting@gmail.com>
2015-07-25 16:03:35 +1200

    Pulling data from GitHub and adding try-catch blocks

--- src/resources/projects.py
+++ src/resources/projects.py
@@ -1,17 +1,35 @@
-from flask.ext.restful import Resource
+from flask.ext.restful import Resource, abort
 from flask import jsonify
+import requests
 
 
-class Projects(Resource):
+class GitHub(Resource):
 	
 	def get(self):
-		return jsonify({'days/units' : 'm days and n units'})
+		"""
+		This method makes a call to the standard GitHub API and simply
+		forwards the response.
+
+		This returns a '504 - Gateway Timeout' on error!
+		"""
+		try:
+			response = requests.get('https://api.github.com/users/svyzz').json()
+		except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+			abort(504)
+
+		return response
+
 
 	def post(self, **kwargs):
+		"""
+    	Exit with a 405 since this method is disallowed for this resource
+		"""
 		abort(405)
+
 
 	def put(self, **kwargs):
 		abort(405)
+
 
 	def delete(self, **kwargs):
 		abort(405)
